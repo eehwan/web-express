933927094207ace13156ad3db9bb96d42a30b98b
eehwan <eehwan.shin@gmail.com>
2020-07-12 03:27:38 +0900

    working

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
+console.time('timer');
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
 const qs = require('querystring');
 const fs = require('fs');
+
+const template = require('./lib/template.js');
+// const buttons = require('./lib/buttons.js');
+
+// secrets
 const path = require('path');
 const sanitizeHtml = require('sanitize-html');
 
-const template = require('./lib/template.js');
-const buttons = require('./lib/buttons.js');
-
-// middleware
+//application-level middleware
 const bodyParser = require("body-parser");
 const compression = require("compression");
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(compression());
 //self-made middleware
-app.use(function(req,res, next){
+app.get('*', function(req,res, next){
   fs.readdir('./data', function(error, filelist){
     req.list = filelist;
     next();
@@ -30,28 +34,35 @@ app.get('/', function(req, res){
   var list = template.list(req.list);
   var html = template.HTML(title, list,
     `<h2>${title}</h2>${description}`,
-    `${buttons.post}`
+    '<a href="/post">post</a>'
   );
   res.send(html);
 });
 
-//page
-app.get('/page/:page', function(req, res){
+//page-read
+app.get('/page/:page', function(req, res, next){
   var filteredId = path.parse(req.params.page).base;
   fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    var title = req.params.page;
-    var sanitizedTitle = sanitizeHtml(title);
-    var sanitizedDescription = sanitizeHtml(description, {
-      allowedTags:['a']
-    });
-    var list = template.list(req.list);
-    var html = template.HTML(sanitizedTitle, list,
-      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-      ` ${buttons.post}
-        ${buttons.modify}
-        ${buttons.delete}`
-    );
-    res.send(html);
+    if (!err){
+      var sanitizedTitle = sanitizeHtml(filteredId);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['a']
+      });
+      var list = template.list(req.list);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        `<a href="/post">post</a>
+        <a href="/modify/${sanitizedTitle}">modify</a>
+        <form action="/delete_process" method="post">
+      <input type="hidden" name="id" value="${sanitizedTitle}">
+      <input type="submit" value="delete">
+      </form>`
+      );
+      res.send(html);
+    }
+    else{
+      next(err);
+    }
   });
 });
 //post
@@ -84,9 +95,8 @@ app.post('/post_process', function(req,res){
 //modify
 app.get('/modify/:title', function(req,res){
   fs.readdir('./data', function(error, filelist){
-    var filteredTitle = path.parse(req.params.title).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = req.params.title;
+    var title = req.params.title;
+    fs.readFile(`data/${title}`, 'utf8', function(err, description){
       var list = template.list(filelist);
       var html = template.HTML(title, list,
          `
@@ -108,9 +118,9 @@ app.get('/modify/:title', function(req,res){
    });
 });
 //modify_process
-app.post('/modify_process/:id', function(req,res){
+app.post('/modify_process/', function(req,res){
   var query = req.body;
-  var ex_title = query.id;
+  var ex_title = query.ex_title;
   var title = query.title;
   var description = query.description;
   fs.rename(`data/${ex_title}`, `data/${title}`, function(error){
@@ -129,5 +139,24 @@ app.post('/delete_process', function(req,res){
   })
 });
 
+//error_page
+app.use(function(err, req, res, next){
+  console.error(err);
+  // res.status(404).send("<script>\
+  //    alert('page not found');\
+  //    history.go(-1);\
+  //    </script>");
+  res.send(`<script>
+     alert("${err.stack}");
+     history.go(-1);
+     </script>`);
+});
+app.use(function(req, res, next){
+  res.send("<script>\
+     alert('page not found');\
+     history.go(-1);\
+     </script>");
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+console.timeEnd('timer');
